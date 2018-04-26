cda9e4ff8e0bb16a90ddcd8af1a4c2fc676f10af
Ivan Gargov <ivan.gargov@telelink.com>
2018-04-26 08:59:28 +0300

    Minor UoW change.

--- TwitterBackup/TwitterBackup.Data/UnitOfWork/UnitOfWork.cs
+++ TwitterBackup/TwitterBackup.Data/UnitOfWork/UnitOfWork.cs
@@ -20,7 +20,7 @@ namespace TwitterBackup.Data.UnitOfWork
             {
                 if (this.twitterAccounts == null)
                 {
-                    return this.twitterAccounts = new GenericRepository<TwitterAccount>(this.context);
+                    this.twitterAccounts = new GenericRepository<TwitterAccount>(this.context);
                 }
 
                 return this.twitterAccounts;
@@ -33,7 +33,7 @@ namespace TwitterBackup.Data.UnitOfWork
             {
                 if (this.twitterAccountImages == null)
                 {
-                    return this.twitterAccountImages = new GenericRepository<TwitterAccountImage>(this.context);
+                    this.twitterAccountImages = new GenericRepository<TwitterAccountImage>(this.context);
                 }
 
                 return this.twitterAccountImages;
