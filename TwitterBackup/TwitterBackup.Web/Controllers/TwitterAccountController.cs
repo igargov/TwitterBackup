f72577a9b2d285dd4ec32439cb7fb0370afbb339
Petko <petkotihomirovpetkov@gmail.com>
2018-04-28 20:48:59 +0300

    Refactoring TwitterAccountController

--- TwitterBackup/TwitterBackup.Web/Controllers/TwitterAccountController.cs
+++ TwitterBackup/TwitterBackup.Web/Controllers/TwitterAccountController.cs
@@ -35,7 +35,18 @@ namespace TwitterBackup.API.Controllers
         {
             var twitterResult = await this.twitterApiService.RetrieveTwitterAccountAsync(screenName);
 
-            return View();
+            TwitterAccountViewModel twitterAccountViewModel = null;
+
+            //if (!twitterResult.Contains("User not found"))
+            //{
+            //    var jsonSettings = new JsonSerializerSettings();
+            //    jsonSettings.DateFormatString = "ddd MMM dd HH:mm:ss +ffff yyyy";
+            //    twitterAccountViewModel = JsonConvert.DeserializeObject<TwitterAccountViewModel>(twitterResult, jsonSettings);
+            //    twitterAccountViewModel.CreatedAt = DateTime.Now;
+            //    int saveResult = this.twitterAccountService.SaveAccount(twitterAccountViewModel);
+            //}
+
+            return View(twitterAccountViewModel);
         }
     }
 }
