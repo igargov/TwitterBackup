911c51ea968e9e36facfc50a0179d4c1df481a52
Ivan Gargov <ivan.gargov@telelink.com>
2018-05-06 17:12:04 +0300

    GetAll method implemented for TwitterStatus.

--- TwitterBackup/TwitterBackup.Services/Contracts/ITwitterStatusService.cs
+++ TwitterBackup/TwitterBackup.Services/Contracts/ITwitterStatusService.cs
@@ -1,9 +1,13 @@
-﻿using TwitterBackup.TwitterDTOs;
+﻿using System.Collections.Generic;
+using TwitterBackup.Services.ViewModels;
+using TwitterBackup.TwitterDTOs;
 
 namespace TwitterBackup.Services.Contracts
 {
     public interface ITwitterStatusService
     {
+        List<TwitterStatusViewModel> GetAll(int accountId);
+
         int Create(TwitterStatusDTO model);
 
         bool Delete();
--- TwitterBackup/TwitterBackup.Services/TwitterStatusService.cs
+++ TwitterBackup/TwitterBackup.Services/TwitterStatusService.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using TwitterBackup.Data.Models;
 using TwitterBackup.Data.UnitOfWork;
 using TwitterBackup.Providers;
 using TwitterBackup.Services.Contracts;
+using TwitterBackup.Services.ViewModels;
 using TwitterBackup.TwitterDTOs;
 
 namespace TwitterBackup.Services
@@ -17,6 +19,19 @@ namespace TwitterBackup.Services
         {
             this.unitOfWork = unitOfWork;
             this.mapper = mapper;
+        }
+
+        public List<TwitterStatusViewModel> GetAll(int accountId)
+        {
+            var twitterStatuses = this.unitOfWork.TwitterStatuses
+                .All()
+                .Where(ts => ts.TwitterAccountId == accountId);
+
+            var twitterStatusesModel = this.mapper
+                .ProjectTo<TwitterStatusViewModel>(twitterStatuses)
+                .ToList();
+
+            return twitterStatusesModel;
         }
 
         public int Create(TwitterStatusDTO model)
